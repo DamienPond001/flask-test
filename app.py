1a7d390c74495308710b87330bd12b8b88d9e129
Damien <pond@live.co.za>
2019-08-03 17:01:04 +0200

    file creation

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     env_var = 'mufukn test'#os.getenv('ENV_VAR')
+
     return render_template('index.html', env_var=env_var)
+
+@app.route('/write/<filename>')
+def write(filename):
+    f= open("{}.txt".format(filename),"w+")
+
+    return "{} was created".format(filename)
 
 if __name__ == '__main__':
     app.run(port=8080)
