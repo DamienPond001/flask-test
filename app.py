af784909f2239a053418390492a9e17781919a74
DamienPond001 <Damien.Pond@alumni.uct.ac.za>
2019-08-05 16:38:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    env_var = 'exceptionally nice app'#os.getenv('ENV_VAR')
+    env_var = 'mufukn app'#os.getenv('ENV_VAR')
 
     return render_template('index.html', env_var=env_var)
 
